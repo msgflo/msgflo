msgflo/msgflo

commit fee85c422f446c19b2a1705d0673b978c97fbe73

setup: Allow to use a shell for participant commands
Optional and opt-in

src/setup.coffee
@@ -21,10 +21,15 @@ queueName = (c) ->
 startProcess = (cmd, options, callback) ->
   env = process.env
   env['MSGFLO_BROKER'] = options.broker
+  if options.shell
+    prog = options.shell
+    args = ['-c', cmd]
+  else
+    prog = cmd.split(' ')[0]
+    args = cmd.split(' ')
+
   childoptions =
     env: env
-  prog = cmd.split(' ')[0]
-  args = cmd.split(' ').splice(1)
   debug 'participant process start', prog, args.join(' ')
   child = child_process.spawn prog, args, childoptions
   returned = false

commit a68708f366043f16875896e40c36593180308815

setup: Fix command being used twice
Regression in fee85c422f

src/setup.coffee
@@ -26,7 +26,7 @@ startProcess = (cmd, options, callback) ->
     args = ['-c', cmd]
   else
     prog = cmd.split(' ')[0]
-    args = cmd.split(' ')
+    args = cmd.split(' ').splice(1)
 
   childoptions =
     env: env